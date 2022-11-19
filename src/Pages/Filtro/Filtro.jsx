7bd2d5cf01c9c0f1935c67167237e8e5d00d10cb
Bamarcheti <bmarchetifiorin@gmail.com>
2022-11-19 11:58:58 -0400

    corrigindo tela filtro

--- src/Pages/Filtro/Filtro.jsx
+++ src/Pages/Filtro/Filtro.jsx
@@ -1,8 +1,9 @@
+import { Box } from '@mui/material';
 import Slider from '@mui/material/Slider';
 import Stack from '@mui/material/Stack';
 import React, { useState } from "react";
 import Button from "../../Components/Button/Button";
-import '../Filtro/Filtro.css';
+import './Filtro.css';
 
 export default function Filtro() {
   const [distance, setDistance] = useState(100);
@@ -23,59 +24,175 @@ export default function Filtro() {
   };
 
   return (
-    <div className="main">
-      <h1 className="titulo">Filtros</h1>
+    <Box
+      marginTop={2}
+      height={930}
+    >
+      <Stack
+        direction="column"
+        justifyContent="center"
+        alignItems="center"
+        spacing={4}
+      >
+        <h1 className="titulo">Filtros</h1>
 
-      <form>
-        <div className="raio_km">
-          <p className="text_s">Raio em KM</p>
-          <Stack spacing={2} direction="row" sx={{ mb: 1 }} alignItems="center">
-            <Slider aria-label="Volume" value={distance} onChange={handleChange} />
-          </Stack>
-        </div>
+        <Stack
+          direction="column"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <div>
+            <h3 className="text">Raio em KM</h3>
 
-        <p className="text_s">Eu quero adotar</p>
-        <div className="adotar">
-          <div className="top">
-            <Button className="botao_filtro" value={dog} type="dog">Cachorro</Button>
-            <Button className="botao_filtro" value={bird} type="bird">Pássaro</Button>
+            <Slider
+              marginRight={4}
+              sx={{
+                width: 290,
+                color: '#ba4a16',
+              }}
+              value={distance}
+              onChange={handleChange}
+            />
           </div>
-          <div className="down">
-            <Button className="botao_filtro" value={cat} type="cat">Gato</Button>
-            <Button className="botao_filtro" value={mouse} type="other">Roedores</Button>
+        </Stack>
+
+        <Stack
+          direction="column"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <div>
+            <h3 className="text">Eu quero adotar</h3>
+
+            <Stack
+              direction="row"
+              justifyContent="center"
+              alignItems="center"
+              spacing={4}
+            >
+              <Button className="botao_filtro" value={dog} type="dog">Cachorro</Button>
+              <Button className="botao_filtro" value={bird} type="bird">Pássaro</Button>
+            </Stack>
+            <Stack
+              direction="row"
+              justifyContent="center"
+              alignItems="center"
+              spacing={4}
+            >
+              <Button className="botao_filtro" value={cat} type="cat">Gato</Button>
+              <Button className="botao_filtro" value={mouse} type="other">Roedores</Button>
+            </Stack>
+            <Stack
+              direction="row"
+              justifyContent="center"
+              alignItems="center"
+              spacing={4}
+            >
+              <Button className="botao_filtro" value={others} type="race">Outros</Button>
+            </Stack>
           </div>
-          <div className="outros">
-            <Button className="botao_filtro" value={others} type="race">Outros</Button>
+        </Stack>
+
+        <Stack
+          direction="column"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <div>
+            <h3 className="text">Sexo</h3>
+
+            <Stack
+              direction="row"
+              justifyContent="center"
+              alignItems="center"
+              spacing={2}
+            >
+              <Stack
+                direction='row'
+                spacing={2}
+                justifyContent="center"
+                alignItems="center"
+              >
+                <Button className="botao_filtro" value={dog} type="macho">Macho</Button>
+              </Stack>
+              <Button className="botao_filtro" value={dog} type="femea">Fêmea</Button>
+            </Stack>
           </div>
+        </Stack>
 
-        </div>
+        <Stack
+          direction="column"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <div>
+            <h3 className="text">Faixa etária</h3>
 
-        <p className="text_s">Sexo</p>
-        <div className="sexo">
-          <Button className="botao_filtro" value={dog} type="macho">Macho</Button>
-          <Button className="botao_filtro" value={dog} type="femea">Fêmea</Button>
-        </div>
+            <Stack
+              direction="row"
+              justifyContent="center"
+              alignItems="center"
+              spacing={4}
+            >
+              <Button className="botao_filtro" value={filhote} type="filho">Filhote</Button>
+              <Button className="botao_filtro" value={adulto} type="adulto">Adulto</Button>
+            </Stack>
 
-        <p className="text_s">Faixa etária</p>
-        <div className="faixa_etaria">
-          <Button className="botao_filtro" value={filhote} type="filho">Filhote</Button>
-          <Button className="botao_filtro" value={adulto} type="adulto">Adulto</Button>
-          <Button className="botao_filtro" value={idoso} type="idoso">Idoso</Button>
-        </div>
+            <Stack
+              direction="row"
+              justifyContent="center"
+              alignItems="center"
+              spacing={4}
+            >
+              <Button className="botao_filtro" value={idoso} type="idoso">Idoso</Button>
+            </Stack>
+          </div>
+        </Stack>
 
-        <p className="text_s">Porte</p>
-        <div className="porte">
-          <Button className="botao_filtro" value={pequeno} type="pequeno">Pequeno</Button>
-          <Button className="botao_filtro" value={medio} type="medio">Médio</Button>
-          <Button className="botao_filtro" value={grande} type="grande">Grande</Button>
-        </div>
+        <Stack
+          direction="column"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <div>
+            <h3 className="text">Porte</h3>
 
-        <div className="botao_entrar">
-          <Button rel="botao">
-            Entrar
-          </Button>
-        </div>
-      </form>
-    </div>
+            <Stack
+              direction="row"
+              justifyContent="center"
+              alignItems="center"
+              spacing={4}
+            >
+              <Button className="botao_filtro" value={pequeno} type="pequeno">Pequeno</Button>
+              <Button className="botao_filtro" value={medio} type="medio">Médio</Button>
+            </Stack>
+
+            <Stack
+              direction="row"
+              justifyContent="center"
+              alignItems="center"
+              spacing={4}
+            >
+              <Button className="botao_filtro" value={grande} type="grande">Grande</Button>
+            </Stack>
+          </div>
+        </Stack>
+
+        <Stack
+          direction="column"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <div>
+            <Button
+              rel="botao"
+              marginBottom={15}
+            >
+              Entrar
+            </Button>
+          </div>
+        </Stack>
+      </Stack>
+    </Box>
   )
 }
