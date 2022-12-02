4a6ec5d773aa9fff5989d53f57126eac34977364
Rayane <rayaneassis_112@hotmail.com>
2022-12-02 14:07:39 -0400

    fix pages name

--- src/Components/Logo/Logo.jsx
+++ src/Components/Logo/Logo.jsx
@@ -1,9 +1,5 @@
 import React from "react";
-<<<<<<< HEAD
 import logo from "../../assets/logo.png"
-=======
-import logo from "../../assets/logo.png";
->>>>>>> d1b9b723add4816e13b9fc11c0c80497f4b70d09
 
 const Logo = ({alt, width, height, className}) => {
     return(
--- src/Components/Pets/Pets.jsx
+++ src/Components/Pets/Pets.jsx
@@ -1,9 +1,5 @@
 import React from "react";
-<<<<<<< HEAD
 import pets from "../../assets/pets.png"
-=======
-import pets from "../../assets/pets.png";
->>>>>>> d1b9b723add4816e13b9fc11c0c80497f4b70d09
 
 const Pets = ({alt, width, height, className}) => {
     return(
